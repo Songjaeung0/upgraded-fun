4cafb79dcf1aecaadc02b0b52200d0cd63a5cc8a
Songjaeung0 <64520412+Songjaeung0@users.noreply.github.com>
2026-05-12 18:50:20 +0900

    fix: add router basename for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import './styles/global.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
